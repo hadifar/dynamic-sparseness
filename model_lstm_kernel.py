4c68b1b94e95992511a843a3867c014010f338be
Amir Hadifar <amir.hadifar@ugent.be>
2021-07-05 16:10:22 +0200

    update issues

--- model_lstm_kernel.py
+++ model_lstm_kernel.py
@@ -53,6 +53,8 @@ import time
 import numpy as np
 import tensorflow.compat.v1 as tf
 
+tf.compat.v1.disable_eager_execution()
+
 from model_lstm_kernel_ptb_reader import ptb_iterator, ptb_raw_data
 
 flags = tf.flags
@@ -78,16 +80,17 @@ class PTBModel(object):
     def __init__(self, is_training, config):
         self.batch_size = batch_size = config.batch_size
         self.num_steps = num_steps = config.num_steps
-        size = config.hidden_size
+        self.size = config.hidden_size
         vocab_size = config.vocab_size
 
         self._input_data = tf.placeholder(tf.int32, [batch_size, num_steps])
         self._targets = tf.placeholder(tf.int32, [batch_size, num_steps])
+        # self._initial_state = tf.placeholder(tf.float32, [2, batch_size, self.size])
 
         # Slightly better results can be obtained with forget gate biases
         # initialized to 1 but the hyperparameters of the model would need to be
         # different than reported in the paper.
-        lstm_cell = tf.nn.rnn_cell.LSTMCell(size, forget_bias=0.0)
+        lstm_cell = tf.nn.rnn_cell.LSTMCell(self.size, forget_bias=0.0)
         if is_training and config.keep_prob < 1:
             lstm_cell = tf.nn.rnn_cell.DropoutWrapper(
                 lstm_cell, output_keep_prob=config.keep_prob)
@@ -97,7 +100,7 @@ class PTBModel(object):
 
         with tf.device("/cpu:0"):
             embedding = tf.get_variable(
-                "embedding", [vocab_size, size], dtype=data_type())
+                "embedding", [vocab_size, self.size], dtype=data_type())
             inputs = tf.nn.embedding_lookup(embedding, self._input_data)
 
         if is_training and config.keep_prob < 1:
@@ -120,15 +123,17 @@ class PTBModel(object):
                 (cell_output, state) = cell(inputs[:, time_step, :], state)
                 outputs.append(cell_output)
 
-        output = tf.reshape(tf.concat(1, outputs), [-1, size])
+        output = tf.reshape(outputs, [-1, self.size])
         softmax_w = tf.get_variable(
-            "softmax_w", [size, vocab_size], dtype=data_type())
+            "softmax_w", [self.size, vocab_size], dtype=data_type())
         softmax_b = tf.get_variable("softmax_b", [vocab_size], dtype=data_type())
         logits = tf.matmul(output, softmax_w) + softmax_b
-        loss = tf.nn.seq2seq.sequence_loss_by_example(
-            [logits],
-            [tf.reshape(self._targets, [-1])],
-            [tf.ones([batch_size * num_steps], dtype=data_type())])
+        loss = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=tf.reshape(self._targets, [-1]), logits=logits)
+        # loss = tf.nn.seq2seq.sequence_loss_by_example(
+        #     [logits],
+        #     [],
+        #     [tf.ones([batch_size * num_steps], dtype=data_type())])
+
         self._cost = cost = tf.reduce_sum(loss) / batch_size
         self._final_state = state
 
@@ -186,7 +191,7 @@ class SmallConfig(object):
     num_layers = 2
     num_steps = 20
     hidden_size = 200
-    max_epoch = 4
+    max_epoch = 1
     max_max_epoch = 13
     keep_prob = 1.0
     lr_decay = 0.5
@@ -248,7 +253,10 @@ def run_epoch(session, m, data, eval_op, verbose=False):
     start_time = time.time()
     costs = 0.0
     iters = 0
-    state = m.initial_state.eval()
+    state = session.run(m._initial_state)
+    # state = (np.zeros([m.batch_size, m.size], dtype=np.float32),
+    #          np.zeros([m.batch_size, m.size], dtype=np.float32))
+
     for step, (x, y) in enumerate(ptb_iterator(data, m.batch_size,
                                                m.num_steps)):
         cost, state, _ = session.run([m.cost, m.final_state, eval_op],
@@ -300,15 +308,14 @@ def main(_):
             mvalid = PTBModel(is_training=False, config=config)
             mtest = PTBModel(is_training=False, config=eval_config)
 
-        tf.initialize_all_variables().run()
+        tf.global_variables_initializer().run()
 
         for i in range(config.max_max_epoch):
             lr_decay = config.lr_decay ** max(i - config.max_epoch, 0.0)
             m.assign_lr(session, config.learning_rate * lr_decay)
 
             print("Epoch: %d Learning rate: %.3f" % (i + 1, session.run(m.lr)))
-            train_perplexity = run_epoch(session, m, train_data, m.train_op,
-                                         verbose=True)
+            train_perplexity = run_epoch(session, m, train_data, m.train_op, verbose=True)
             print("Epoch: %d Train Perplexity: %.3f" % (i + 1, train_perplexity))
             valid_perplexity = run_epoch(session, mvalid, valid_data, tf.no_op())
             print("Epoch: %d Valid Perplexity: %.3f" % (i + 1, valid_perplexity))
